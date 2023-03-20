d9c67d31c84786ea3c4374572c83e7507121afc7
Ajay Sharma <gs.ajaysharma@gmail.com>
2023-03-20 11:31:50 +0000

    compressed images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
                 <h3>University Ultra Run</h3>
                 <p>Be A Part Of History</p>
                 <ul className='footer-socials'>
-                    <li><a href="#"><img src={Tiktok} alt="tiktok_img"/></a></li>
-                    <li><a href="#"><img src="https://i.ibb.co/ySwtH4B/instagram.png" alt='instagram' /></a></li>
+                    <li ><a target="_blank" href="https://www.tiktok.com/@universityultrarun?_t=8amEsPxw6yb&_r=1"><img src={Tiktok} alt="tiktok_img"/></a></li>
+                    <li ><a target="_blank" href="https://instagram.com/universityultrarun?igshid=YmMyMTA2M2Y="><img src="https://i.ibb.co/ySwtH4B/instagram.png" alt='instagram' /></a></li>
                 </ul>
             </div>
             <div className='footer-bottom'>
